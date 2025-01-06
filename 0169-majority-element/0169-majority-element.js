c875c186a0688a5bef58a864e5a87f8b52d38e69
ajithvraj <ajithvraj007@gmail.com>
2025-01-06 10:11:29 +0530

    Time: 4 ms (62.36%), Space: 52.7 MB (45.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+
+    nums.sort((a,b) => a -b)
+
+    return nums[Math.floor(nums.length / 2)];
+
+
+    
+};
